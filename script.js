f3bbce69b731e8b5f76ce63d3f5c8a27f7802d42
Mauricio Anolin <mauriantolin5@gmail.com>
2023-06-21 23:52:04 -0300

    Final

---
+++ script.js
@@ -0,0 +1,46 @@
+const funcionEsPrimo = (numero) => {
+	let esPrimo = true;
+	for (let i = 2; i < numero; i++) {
+		if (numero % i === 0) {
+			esPrimo = false;
+		}
+	}
+	return esPrimo;
+};
+
+// {
+//   "explorer.confirmDelete": false,
+//   "eslint.codeActionsOnSave.rules": null,
+//   "editor.codeActionsOnSave": {
+//     "source.fixAll.eslint": true
+//   },
+//   "eslint.alwaysShowStatus": true,
+//   "eslint.format.enable": true,
+//   "editor.fontFamily": "'Cascadia Code', 'Courier New', monospace",
+//   "editor.fontLigatures": true,
+//   "window.zoomLevel": 2,
+//   "security.workspace.trust.untrustedFiles": "open",
+//   "workbench.colorTheme": "Ayu One Dark Pro | Mirage Bordered",
+//   "[python]": {
+//     "editor.formatOnType": true
+//   },
+//   "notebook.cellToolbarLocation": {
+//     "default": "right",
+//     "jupyter-notebook": "right"
+//   },
+//   "workbench.iconTheme": "material-icon-theme",
+//   "github.copilot.enable": {
+//     "*": true,
+//     "plaintext": false,
+//     "markdown": false,
+//     "scminput": false
+//   },
+//   "github.copilot.advanced": {},
+//   "editor.defaultFormatter": "esbenp.prettier-vscode",
+//   "editor.inlineSuggest.enabled": true,
+//   "editor.indentSize": "tabSize",
+//   "editor.formatOnSave": true,
+//   "editor.formatOnType": true,
+//   "editor.fontWeight": "normal",
+//   "editor.formatOnPaste": true
+// }
